41fc5dff5cd6d42d761a94fb7052e5e834c4bf48
MyName <2702@2702.com>
2016-01-21 16:17:35 -0500

    stuff compiles, doesn't link

---
+++ 2016/webcam/main.cpp
@@ -0,0 +1,23 @@
+#include "opencv2/opencv.hpp"
+#include "../opencv_lib/2702_proc.h"
+
+using namespace cv;
+int main(int argc, char** argv)
+{
+    VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0))
+        return 0;
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame;
+        if( frame.empty() ) break; // end of video stream
+        process (&frame);
+
+    }
+    // the camera will be closed automatically upon exit
+    // cap.close();
+    return 0;
+}
