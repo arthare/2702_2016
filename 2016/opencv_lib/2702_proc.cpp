b30b1988cfd78b9c015366e194afa47b67d37d85
Benjamin McDonnell <benjaminjames@sympatico.ca>
2016-01-25 09:47:50 -0500

    organized code for fixing up the image with erode and dialate

--- 2016/opencv_lib/2702_proc.cpp
+++ 2016/opencv_lib/2702_proc.cpp
@@ -9,35 +9,38 @@ using namespace cv;
 
 using namespace std;
 
-pos HSV_convert(Mat* img, int* args)
+Mat outputImage;
+
+pos fixImage (Mat inputImage)
 {
     int erodeValue = 3;
     int dilatevalue = 3;
-    Mat hsvconvert;
-    Mat hsvconvert2;
-    Mat hsvconvertE;
-    Mat hsvconvertD;
+
     Mat erodeElement = getStructuringElement(MORPH_RECT, Size(erodeValue,erodeValue));
     Mat dilateElement = getStructuringElement(MORPH_RECT, Size(dilatevalue,dilatevalue));
+
+    dilate(inputImage, inputImage, dilateElement);
+    erode(inputImage, inputImage, erodeElement);
+    erode(inputImage, outputImage, erodeElement);
+}
+
+pos HSV_convert(Mat* img, int* args)
+{
+    Mat hsvconvert;
+    Mat hsvconvert2;
     cvtColor(*img, hsvconvert, CV_BGR2HSV);
     if (args)
     {
 
         inRange(hsvconvert, Scalar(args[0], args[1], args[2]), Scalar(args[3], args[4], args[5]), hsvconvert2);
-        dilate(hsvconvert2, hsvconvert2, dilateElement);
-        erode(hsvconvert2, hsvconvert2, erodeElement);
-        erode(hsvconvert2, hsvconvert2, erodeElement);
-        dilate(hsvconvert2, hsvconvert2, dilateElement);
-        imshow("window", hsvconvert2);
+        fixImage(hsvconvert2);
+        imshow("window", outputImage);
     }
     else
     {
         inRange(hsvconvert, cv::Scalar(54, 89, 32), cv::Scalar(91, 255, 255), hsvconvert2);
-        dilate(hsvconvert2, hsvconvert2, dilateElement);
-        erode(hsvconvert2, hsvconvert2, erodeElement);
-        erode(hsvconvert2, hsvconvert2, erodeElement);
-        dilate(hsvconvert2, hsvconvert2, dilateElement);
-        imshow("window", hsvconvert2);
+        fixImage(hsvconvert2);
+        imshow("window",outputImage);
     }
     //threshold( hsvconvert, hsvconvert2, 1, 1, 1 );
 
